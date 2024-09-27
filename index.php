fe2d1d721eb89f2e2d42d974d0134ddddb232245
Heberth Matos <heberthhm.hm@outlook.com>
2024-09-26 21:08:04 -0300

    stylesadd

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Ab Initio</title>
     <!-- favicon -->
     <link rel="icon" href="/img/logo1.ico">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styles.css">
     <!-- icons social-media -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
