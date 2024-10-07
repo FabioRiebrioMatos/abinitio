f68c658defb9f2b8280ab41300cfa49bf6ce42aa
Fabio Matos <aftecfabio@gmail.com>
2024-10-07 15:46:39 -0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 <footer id="footer">
     <p>&copy; 2024 Ab Initio. Todos os direitos reservados.</p>
     <div class="social-media">
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-whatsapp"></i></a>
+        <a href="#"><i class="fab fa-instagram"> <strong>@ AbInitio</strong></i></a>
+        <a href="#"><i class="fab fa-whatsapp"> <strong>(79) 99830-1980</strong></i></a>
+        <a href="../about.php"><i class="fas fa-user"> <strong>Sobre Nós<strong></i></a>
     </div>
 </footer>
