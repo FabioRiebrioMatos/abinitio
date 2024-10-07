7772a3dc1da1883da7f6cdd4ceae77e9ca6e2d57
Heberth Matos <heberthhm.hm@outlook.com>
2024-10-07 15:30:30 -0300

    Att about

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <footer id="footer">
     <p>&copy; 2024 Ab Initio. Todos os direitos reservados.</p>
     <div class="social-media">
-        <a href="#"><i class="fab fa-instagram"> @ AbInitio</i></a>
-        <a href="#"><i class="fab fa-whatsapp"> (79) 99830-1980</i></a>
-        <a href="../about.php"><i class="fas fa-user"> Sobre Nós</i></a>
+        <a href="#"><i class="fab fa-instagram"> <strong>@ AbInitio</strong></i></a>
+        <a href="#"><i class="fab fa-whatsapp"> <strong>(79) 99830-1980</strong></i></a>
+        <a href="../about.php"><i class="fas fa-user"> <strong>Sobre Nós<strong></i></a>
     </div>
 </footer>
