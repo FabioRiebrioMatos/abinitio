f7cda1ffc611424c9e5ca2d1a09e018ae145d2e8
Fabio Matos <aftecfabio@gmail.com>
2024-10-10 11:04:42 -0300

    atualizacao

--- index.php
+++ index.php
@@ -38,20 +38,13 @@
             <div class="section-text">
                 <h2>QUAL É OBJETIVO DA MENTORIA?
                 </h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut enim placerat, placerat nisl
-                    interdum, molestie ligula. Pellentesque a enim quis quam imperdiet commodo quis a erat. Mauris
-                    suscipit enim eget neque ornare, eget varius sapien facilisis. Nunc lectus lectus, pharetra sed enim
-                    sit amet, ornare pellentesque dui. Vestibulum viverra mauris magna. Phasellus volutpat dictum quam,
-                    eu vehicula ligula tincidunt eu. Cras arcu velit, molestie id est quis, scelerisque suscipit velit.
-                    Aenean pellentesque nibh sit amet est cursus commodo. Donec ac eros et ipsum imperdiet efficitur.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec convallis massa a nibh varius
-                    cursus. Nunc augue eros, tristique a mi ac, molestie tincidunt libero. Sed tempor ac dui quis
-                    pretium. Donec posuere ornare ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-                    Maecenas in libero nunc. Quisque efficitur quam in leo mollis, in rhoncus ante auctor. Pellentesque
-                    habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus facilisis
-                    elit et nulla fermentum elementum. Nam mi sapien, tempus sit amet scelerisque at, lacinia non diam.
-                    Donec ut consequat lectus. Etiam ac pellentesque urna, ut varius orci.</p>
+                <p>Atravessar a ponte entre universidade e fórum do momento em que se encontra (estagiário, servidor, advogado); <br>
+                    Tratar sobre temas práticos que não são abordados durante a graduação;
+                    Apresentar a realidade interna dos sistemas jurídicos;
+                    Identificar a competência de cada posição ocupada, noutros termos, identificar cada um em seu quadrado;
+                    Apresentar um espaço de aprendizagem e acolhimento, demonstrando que, apesar das dificuldades que atravessam cada caminho, existem possiblidades de crescimento através da cordialidade, educação, bom senso e, sobretudo, conhecimento;
+                    Enfatizar que não há como burlar o início da caminhada, é importante ter em mente que antes de caminhar é preciso engatinhar e, antes de correr, é preciso caminhar. Toda fase é importante e agrega ao objetivo final que é o domínio da realidade/prática jurídica;
+                    Demonstrar que existe um “delay” entre a lei, teoria e a realidade nua e crua. O judiciário não consegue chegar dentro da sua casa e/ou dentro da sua empresa.</p>
             </div>
         </section>
 
