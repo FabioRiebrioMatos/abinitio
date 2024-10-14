874b2042efce9f12ce3630c4c18c1253c35389d6
Fabio Matos <aftecfabio@gmail.com>
2024-10-14 12:18:41 -0300

    textos

--- index.php
+++ index.php
@@ -40,11 +40,11 @@
                 <h2>QUAL É OBJETIVO DA MENTORIA?
                 </h2>
                 <p>Atravessar a ponte entre universidade e fórum do momento em que se encontra (estagiário, servidor, advogado); <br>
-                    Tratar sobre temas práticos que não são abordados durante a graduação;
-                    Apresentar a realidade interna dos sistemas jurídicos;
-                    Identificar a competência de cada posição ocupada, noutros termos, identificar cada um em seu quadrado;
-                    Apresentar um espaço de aprendizagem e acolhimento, demonstrando que, apesar das dificuldades que atravessam cada caminho, existem possiblidades de crescimento através da cordialidade, educação, bom senso e, sobretudo, conhecimento;
-                    Enfatizar que não há como burlar o início da caminhada, é importante ter em mente que antes de caminhar é preciso engatinhar e, antes de correr, é preciso caminhar. Toda fase é importante e agrega ao objetivo final que é o domínio da realidade/prática jurídica;
+                    Tratar sobre temas práticos que não são abordados durante a graduação;<br>
+                    Apresentar a realidade interna dos sistemas jurídicos;<br>
+                    Identificar a competência de cada posição ocupada, noutros termos, identificar cada um em seu quadrado;<br>
+                    Apresentar um espaço de aprendizagem e acolhimento, demonstrando que, apesar das dificuldades que atravessam cada caminho, existem possiblidades de crescimento através da cordialidade, educação, bom senso e, sobretudo, conhecimento;<br>
+                    Enfatizar que não há como burlar o início da caminhada, é importante ter em mente que antes de caminhar é preciso engatinhar e, antes de correr, é preciso caminhar.<br>Toda fase é importante e agrega ao objetivo final que é o domínio da realidade/prática jurídica;<br>
                     Demonstrar que existe um “delay” entre a lei, teoria e a realidade nua e crua. O judiciário não consegue chegar dentro da sua casa e/ou dentro da sua empresa.</p>
             </div>
         </section>
@@ -53,21 +53,16 @@
         <section class="content-section right">
             <img src="/img/post2.jpg" alt="Img" class="section-image">
             <div class="section-text">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut enim placerat, placerat nisl
-                    interdum, molestie ligula. Pellentesque a enim quis quam imperdiet commodo quis a erat. Mauris
-                    suscipit enim eget neque ornare, eget varius sapien facilisis. Nunc lectus lectus, pharetra sed enim
-                    sit amet, ornare pellentesque dui. Vestibulum viverra mauris magna. Phasellus volutpat dictum quam,
-                    eu vehicula ligula tincidunt eu. Cras arcu velit, molestie id est quis, scelerisque suscipit velit.
-                    Aenean pellentesque nibh sit amet est cursus commodo. Donec ac eros et ipsum imperdiet efficitur.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec convallis massa a nibh varius
-                    cursus. Nunc augue eros, tristique a mi ac, molestie tincidunt libero. Sed tempor ac dui quis
-                    pretium. Donec posuere ornare ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-                    Maecenas in libero nunc. Quisque efficitur quam in leo mollis, in rhoncus ante auctor. Pellentesque
-                    habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus facilisis
-                    elit et nulla fermentum elementum. Nam mi sapien, tempus sit amet scelerisque at, lacinia non diam.
-                    Donec ut consequat lectus. Etiam ac pellentesque urna, ut varius orci.</p>
+                <h2>MENTORIA INDIVIDUALIZADA.</h2>
+                <p><strong>A AB INITIO</strong> oferece um programa de mentoria personalizado e exclusivo, projetado para impulsionar a carreira de estudantes de Direito e advogados recém-formados.<br>
+                Através de um acompanhamento individualizado, você terá acesso a um mentor experiente que o guiará em todas as etapas da sua jornada profissional.<br>
+                <strong>Benefícios da mentoria individualizada:</strong><br>
+                <strong>Desenvolvimento personalizado:</strong> Seu mentor irá criar um plano de desenvolvimento específico para atender às suas necessidades e objetivos.<br>
+                <strong>Aumento da confiança:</strong> Com o apoio e o feedback do seu mentor, você se sentirá mais seguro para enfrentar os desafios da advocacia.<br>
+                <strong>Networking estratégico:</strong> Construa relacionamentos sólidos com outros profissionais do Direito e expanda sua rede de contatos.<br>
+                <strong>Desenvolvimento de habilidades essenciais:</strong> Aprimore suas habilidades técnicas e comportamentais, como negociação, comunicação eficaz e gestão do tempo.
+                Preparação para o mercado de trabalho: Receba orientações práticas sobre como se destacar no mercado de trabalho e construir uma carreira de sucesso.
+                </p>
             </div>
         </section>
 
@@ -75,21 +70,17 @@
                 <section class="content-section left">
             <img src="/img/post3.jpg" alt="Img" class="section-image">
             <div class="section-text">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut enim placerat, placerat nisl
-                    interdum, molestie ligula. Pellentesque a enim quis quam imperdiet commodo quis a erat. Mauris
-                    suscipit enim eget neque ornare, eget varius sapien facilisis. Nunc lectus lectus, pharetra sed enim
-                    sit amet, ornare pellentesque dui. Vestibulum viverra mauris magna. Phasellus volutpat dictum quam,
-                    eu vehicula ligula tincidunt eu. Cras arcu velit, molestie id est quis, scelerisque suscipit velit.
-                    Aenean pellentesque nibh sit amet est cursus commodo. Donec ac eros et ipsum imperdiet efficitur.
-                    Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec convallis massa a nibh varius
-                    cursus. Nunc augue eros, tristique a mi ac, molestie tincidunt libero. Sed tempor ac dui quis
-                    pretium. Donec posuere ornare ipsum. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-
-                    Maecenas in libero nunc. Quisque efficitur quam in leo mollis, in rhoncus ante auctor. Pellentesque
-                    habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus facilisis
-                    elit et nulla fermentum elementum. Nam mi sapien, tempus sit amet scelerisque at, lacinia non diam.
-                    Donec ut consequat lectus. Etiam ac pellentesque urna, ut varius orci.</p>
+                <h2>SUA CARREIRA JURÍDICA ONDE ESTIVER.</h2>
+                <p><strong>A AB INITIO</strong> oferece uma plataforma de mentoria online inovadora, que permite que você participe do programa de qualquer lugar do mundo. Através de videoconferências, chats e materiais didáticos online, você terá acesso a todos os recursos da mentoria, sem a necessidade de se deslocar.<br>
+                <strong>Vantagens da mentoria online:</strong><br>
+                <Strong>Flexibilidade:</Strong> Adapte seus horários de estudo às suas necessidades e concilie a mentoria com sua rotina.<br>
+                <strong>Acessibilidade:</strong> Participe do programa de qualquer lugar que tenha acesso à internet.<br>
+                <strong>Recursos digitais:</strong> Tenha acesso a uma biblioteca virtual com materiais exclusivos sobre diversas áreas do Direito.<br>
+                <strong>Treinamentos online:</strong> Participe de eventos e treinamentos ao vivo ou gravados, com temas relevantes para a sua carreira.<br>
+                Se você busca impulsionar sua carreira jurídica e alcançar seus objetivos profissionais, a AB INITIO é a solução ideal.<br>
+                <br>
+                <strong>Entre em contato conosco e descubra como a mentoria AB INITIO pode transformar sua vida profissional.</strong>
+                </p>
             </div>
         </section>
 
