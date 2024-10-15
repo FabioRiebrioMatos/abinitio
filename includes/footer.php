0798f4a4a84a3852f12ee721a564c141eddf529f
Heberth Matos <heberthhm.hm@outlook.com>
2024-10-15 12:51:24 -0300

    1

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer id="footer">
     <p>&copy; 2024 Ab Initio. Todos os direitos reservados.</p>
     <div class="social-media">
-        <a target="onclick" href="https://www.instagram.com/abinitio_mentoria/"><i class="fab fa-instagram"> <strong>@ abinitio_mentoria</strong></i></a>
+        <a target="onclick" href="https://www.instagram.com/abinitio_mentoria/"><i class="fab fa-instagram"> <strong>@abinitio_mentoria</strong></i></a>
         <a target="onclick" href="https://wa.me/+5579991354949/"><i class="fab fa-whatsapp"> <strong>(79) 99135-4949</strong></i></a>
         <a href="../about.php"><i class="fas fa-user"> <strong>Sobre Nós<strong></i></a>
     </div>
